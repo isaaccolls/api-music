450d301bf60fc6202ff9c2fc86efcf9b7694fd12
Isaac Colls <colls_isaac@hotmail.com>
2020-02-23 19:10:50 -0300

    edit user

--- controllers/user.js
+++ controllers/user.js
@@ -82,8 +82,26 @@ function loginUser(req, res) {
     });
 }
 
+function updateUser(req, res) {
+    var userId = req.params.id;
+    var update = req.body;
+
+    User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+        if (err) {
+            res.status(504).send({message: '🙃 Updating user error..!!'});
+        } else {
+            if (!userUpdated) {
+                res.status(504).send({message: "😔 user couldn't be updated..!!"});
+            } else {
+                res.status(200).send({user: userUpdated});
+            }
+        }
+    });
+}
+
 module.exports = {
     test,
     saveUser,
     loginUser,
+    updateUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -10,5 +10,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/testing-user-controller', md_auth.ensureAuth, UserController.test);
 api.post('/register', UserController.saveUser);
 api.post('/login', UserController.loginUser);
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser);
 
 module.exports = api;
