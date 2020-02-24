0efac6739ad6ab5bf62b22838682936b69a76d61
Isaac Colls <colls_isaac@hotmail.com>
2020-02-23 21:06:03 -0300

    create artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -11,6 +11,28 @@ function getArtist(req, res) {
     res.status(200).send({message: "getArtists controller"});
 }
 
+function saveArtist(req, res) {
+    var artist = new Artist();
+
+    var params = req.body;
+    artist.name = params.name;
+    artist.description = params.description;
+    artist.image = 'null';
+
+    artist.save((err, artistStored) => {
+        if (err) {
+            res.status(500).send({message: "🙃 Saving artist error..!!"});
+        } else {
+            if (!artistStored) {
+                res.status(404).send({message: "🙃 Artist doesn't saved..!!"});
+            } else {
+                res.status(200).send({artist: artistStored});
+            }
+        }
+    });
+}
+
 module.exports = {
     getArtist,
+    saveArtist,
 };
--- routes/artist.js
+++ routes/artist.js
@@ -8,5 +8,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/artist', md_auth.ensureAuth, ArtistController.getArtist);
+api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 
 module.exports = api;
