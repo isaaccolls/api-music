0e03c30a1de65b82d7d0be4e563343cc531aba5a
Isaac Colls <colls_isaac@hotmail.com>
2020-03-01 22:04:04 -0300

    upload and get artist image

--- controllers/artist.js
+++ controllers/artist.js
@@ -122,10 +122,57 @@ function saveArtist(req, res) {
     });
 }
 
+function uploadImage(req, res) {
+    var artistId = req.params.id;
+    var file_name = 'not loaded...';
+
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\/');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');
+        var file_ext = ext_split[1];
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif') {
+            Artist.findByIdAndUpdate(artistId, {image: file_name}, (err, artistUpdated) => {
+                if (err) {
+                    res.status(504).send({message: '🙃 updating artist error..!!'});
+                } else {
+                    if (!artistUpdated) {
+                        res.status(504).send({message: "😔 artist couldn't be updated..!!"});
+                    } else {
+                        res.status(200).send({artist: artistUpdated});
+                    }
+                }
+            });
+        } else {
+            res.status(200).send({message: '🙈 Not valid file extension..!!'});
+        }
+    } else {
+        res.status(200).send({message: '🙄 Missing image..!!'});
+    }
+}
+
+function getImageFile(req, res) {
+    var imageFile = req.params.imageFile;
+    var path_file = './uploads/artists/' + imageFile;
+
+    fs.exists(path_file, function(exists) {
+        if (exists) {
+            res.status(200).sendFile(path.resolve(path_file));
+        } else {
+            res.status(200).send({message: "🙄 Image doesn't exist..!!"});
+        }
+    });
+}
+
+
 module.exports = {
     getArtist,
     saveArtist,
     getArtists,
     updateArtist,
     deleteArtist,
+    uploadImage,
+    getImageFile,
 };
--- routes/artist.js
+++ routes/artist.js
@@ -7,10 +7,15 @@ var api = express.Router();
 
 var md_auth = require('../middlewares/authenticated');
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({ uploadDir: './uploads/artists' });
+
 api.get('/artist/:id', md_auth.ensureAuth, ArtistController.getArtist);
 api.post('/artist', md_auth.ensureAuth, ArtistController.saveArtist);
 api.get('/artists/:page?', md_auth.ensureAuth, ArtistController.getArtists);
 api.put('/artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
 api.delete('/artist/:id', md_auth.ensureAuth, ArtistController.deleteArtist);
+api.post('/upload-artist-image/:id', [md_auth.ensureAuth, md_upload], ArtistController.uploadImage);
+api.get('/get-artist-image/:imageFile', ArtistController.getImageFile);
 
 module.exports = api;
