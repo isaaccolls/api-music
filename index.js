b08b8e9ff5c92e52d8d0ab2761c778931c04942d
Isaac Colls <colls_isaac@hotmail.com>
2020-03-17 18:36:05 -0300

    log error when connecting DB

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var port = process.env.PORT || 3977;
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/music_db', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err, res) => {
     if (err) {
+        console.error("🙃 something went wrong when connecting DB");
         throw err;
     } else {
         console.log("👽 DB connected..!!");
